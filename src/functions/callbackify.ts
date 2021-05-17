ab0f4cbf4c65bdba09464fab15ec32d1e9e37d47
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-05-17 14:19:04 +0000

    refactor: stop using variadic tuple types

--- src/functions/callbackify.ts
+++ src/functions/callbackify.ts
@@ -1,9 +1,33 @@
+// TypeScript v4.x feature variadic tuple types is not work for `callbackify`:
+// - https://github.com/microsoft/TypeScript/issues/35641
+// - https://github.com/microsoft/TypeScript/issues/39944
+
 type Callback<T> = (err: any, result?: T) => void
 
-export function callbackify<Result, Args extends any[] = unknown[]>(
-  fn: (...args: Args) => PromiseLike<Result>
-): (...args: [...args: Args, callback: Callback<Result>]) => void {
-  return function (...args: [...args: Args, callback: Callback<Result>]): void {
+export function callbackify<Result, T1>(
+  fn: (arg1: T1) => Promise<Result>
+): (arg1: T1, callback: Callback<Result>) => void
+export function callbackify<Result, T1, T2>(
+  fn: (arg1: T1, args2: T2) => Promise<Result>
+): (arg1: T1, args2: T2, callback: Callback<Result>) => void
+export function callbackify<Result, T1, T2, T3>(
+  fn: (arg1: T1, args2: T2, args3: T3) => Promise<Result>
+): (arg1: T1, args2: T2, args3: T3, callback: Callback<Result>) => void
+export function callbackify<Result, T1, T2, T3, T4>(
+  fn: (arg1: T1, args2: T2, args3: T3, args4: T4) => Promise<Result>
+): (arg1: T1, args2: T2, args3: T3, args4: T4, callback: Callback<Result>) => void
+export function callbackify<Result, T1, T2, T3, T4, T5>(
+  fn: (arg1: T1, args2: T2, args3: T3, args4: T4, args5: T5) => Promise<Result>
+): (arg1: T1, args2: T2, args3: T3, args4: T4, args5: T5, callback: Callback<Result>) => void
+export function callbackify<Result, T1, T2, T3, T4, T5, T6>(
+  fn: (arg1: T1, args2: T2, args3: T3, args4: T4, args5: T5, args6: T6) => Promise<Result>
+): (arg1: T1, args2: T2, args3: T3, args4: T4, args5: T5, args6: T6, callback: Callback<Result>) => void
+export function callbackify<Result, T1, T2, T3, T4, T5, T6, T7>(
+  fn: (arg1: T1, args2: T2, args3: T3, args4: T4, args5: T5, args6: T6, args7: T7) => Promise<Result>
+): (arg1: T1, args2: T2, args3: T3, args4: T4, args5: T5, args6: T6, args7: T7, callback: Callback<Result>) => void
+export function callbackify<Result, Args extends any[] = unknown[]>(fn: (...args: Args) => PromiseLike<Result>): (...args: Args) => void
+export function callbackify<Result, Args extends any[] = unknown[]>(fn: (...args: Args) => PromiseLike<Result>): (...args: Args) => void {
+  return function (...args: Args): void {
     const realArgs = args.slice(0, -1) as Args
     const cb = args[args.length - 1] as Callback<Result>
 
