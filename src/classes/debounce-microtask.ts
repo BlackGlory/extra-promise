95a6de8fe8e4b9f790d802d36df7d73cb7583478
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-09-19 13:59:30 +0000

    docs: add comments

--- src/classes/debounce-microtask.ts
+++ src/classes/debounce-microtask.ts
@@ -7,6 +7,8 @@ export class DebounceMicrotask {
     this.#registry.add(fn)
     queueMicrotask(() => {
       if (this.#registry.has(fn)) {
+        // DebounceMicrotask的目的仅仅是避免多次调用queueMicrotaskh函数,
+        // 因此无论fn是同步函数还是异步函数都不会造成影响.
         try {
           fn()
         } finally {
