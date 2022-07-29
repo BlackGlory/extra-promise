e9105f93c0bc97615f0c037de19f0de2e1ea7344
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-07-29 08:04:06 +0000

    docs: modify comments

--- src/functions/throttle-until-done.ts
+++ src/functions/throttle-until-done.ts
@@ -1,5 +1,7 @@
-// 相当于throttleConcurrency(1, fn)的特殊版本, 它会在多次调用中返回首次调用时返回的Promise.
-export function throttleUntilDone<T>(fn: () => PromiseLike<T>): () => Promise<T> {
+// 相当于reusePendingPromise<T>(() => PromiseLike<T>), 只是没有序列化参数
+export function throttleUntilDone<T>(
+  fn: () => PromiseLike<T>
+): () => Promise<T> {
   let pending: Promise<T> | undefined
 
   return function (this: unknown): Promise<T> {
