b6bc00f4d8ce9d947b26b815b64a2ebbf02798a5
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-08-02 17:21:09 +0000

    chore: upgrade dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,5 +15,7 @@ module.exports = {
   , '@typescript-eslint/no-inferrable-types': 'off'
   , '@typescript-eslint/ban-types': 'off'
   , '@typescript-eslint/ban-ts-comment': 'off'
+  , '@typescript-eslint/no-explicit-any': 'off'
+  , '@typescript-eslint/no-unused-vars': 'off'
   }
 }
