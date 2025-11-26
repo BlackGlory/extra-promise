39ce30573012593e0c533339ce27f066e8e28abb
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2025-11-26 06:06:03 +0000

    test(delay): improve

--- __tests__/functions/delay.spec.ts
+++ __tests__/functions/delay.spec.ts
@@ -1,51 +1,45 @@
 import { delay } from '@functions/delay.js'
 import { advanceTimersByTime } from '@test/utils.js'
-import { StatefulPromise } from '@classes/stateful-promise.js'
+import { StatefulPromise, StatefulPromiseState } from '@classes/stateful-promise.js'
 import { AbortController, AbortError } from 'extra-abort'
 import { getErrorPromise } from 'return-style'
 
 describe('delay', () => {
   test('The promise was resolved after timeout', async () => {
-    const setTimeout = vi.spyOn(globalThis, 'setTimeout')
-    const ms = 500
+    const timeout = 500
 
-    const result = delay(ms)
-    const promise = StatefulPromise.from(result)
-
-    expect(setTimeout).toBeCalledTimes(1)
-    expect(setTimeout).toBeCalledWith(expect.any(Function), ms)
-    expect(promise.isPending()).toBe(true)
+    const promise = delay(timeout)
+    const statefulPromise = StatefulPromise.from(promise)
+    const state1 = statefulPromise.state
     await advanceTimersByTime(500)
-    expect(promise.isFulfilled()).toBe(true)
-    expect(await result).toBeUndefined()
+    const state2 = statefulPromise.state
+    const result = await promise
+
+    expect(state1).toBe(StatefulPromiseState.Pending)
+    expect(state2).toBe(StatefulPromiseState.Fulfilled)
+    expect(result).toBeUndefined()
   })
 
   describe('with a signal', () => {
     test('The signal was aborted before calling', async () => {
       const controller = new AbortController()
       controller.abort()
-      const setTimeout = vi.spyOn(globalThis, 'setTimeout')
-      const ms = 500
+      const timeout = 500
 
-      const err = await getErrorPromise(delay(ms, controller.signal))
+      const err = await getErrorPromise(delay(timeout, controller.signal))
 
       expect(err).toBeInstanceOf(AbortError)
-      expect(setTimeout).not.toBeCalled()
     })
 
     test('The signal was aborted after calling', async () => {
       const controller = new AbortController()
-      const setTimeout = vi.spyOn(globalThis, 'setTimeout')
-      const clearTimeout = vi.spyOn(globalThis, 'clearTimeout')
-      const ms = 500
+      const timeout = 500
 
-      const promise = getErrorPromise(delay(ms, controller.signal))
+      const promise = getErrorPromise(delay(timeout, controller.signal))
       queueMicrotask(() => controller.abort())
       const err = await promise
 
       expect(err).toBeInstanceOf(AbortError)
-      expect(setTimeout).toBeCalled()
-      expect(clearTimeout).toBeCalled()
     })
   })
 })
